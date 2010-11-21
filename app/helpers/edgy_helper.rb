52e79635a9c414dc7adf8012ef0061392d86cfc5
Scott Wheeler <scott@directededge.com>
2010-11-21 17:00:01 +0100

    Add in personalized recommendations

--- app/helpers/edgy_helper.rb
+++ app/helpers/edgy_helper.rb
@@ -5,10 +5,14 @@ module EdgyHelper
         Product.edgy_related(Set.new(@order.line_items.map { |i| i.product }), options)
       elsif @product
         @product.edgy_related(options)
+      elsif current_user
+        current_user.edgy_recommended(options)
       else
         []
       end
   end
+
+  alias :edgy_recommended :edgy_related
 
   def edgy_related_table(orientation = :horizontal, options = {})
     render 'shared/edgy_related_table', :orientation => orientation, :options => options
@@ -19,6 +23,9 @@ module EdgyHelper
       "Customers who bought this product also bought:"
     elsif @order
       "Customers who bought these products also bought:"
+    elsif current_user
+      address = current_user.bill_address
+      "Welcome back#{address ? (', ' + address.firstname) : ''}. Here are our recommendations for you:"
     else
       "You might also like:"
     end
